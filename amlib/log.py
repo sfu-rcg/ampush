a8e92239c88211ba64bf658d7c33b5a34808e792
Derek Warren <warren@sfu.ca>
2017-09-14 13:31:24 -0700

    amlib/log.py: log to stdout, too

--- amlib/log.py
+++ amlib/log.py
@@ -1,6 +1,6 @@
 import logging
 from logging import handlers
-import utils, conf
+import sys, utils, conf
 from datetime import datetime
 
 # m = main log
@@ -11,3 +11,10 @@ fmt = logging.Formatter(fmt='%(asctime)s %(message)s',
                         datefmt='%b %e %Y %H:%M:%S')
 m_handler.setFormatter(fmt)
 m.addHandler(m_handler)
+
+# log to stdout, too
+# hat tip: https://stackoverflow.com/a/14058475
+cons = logging.StreamHandler(sys.stdout)
+cons.setLevel(int(conf.c['main_loglevel'])-1)
+cons.setFormatter(fmt)
+m.addHandler(cons)
