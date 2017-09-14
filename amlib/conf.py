9d41a29656e1603d507b08560aed45840ee60387
Derek Warren <warren@sfu.ca>
2017-09-14 13:30:46 -0700

    conf.py: use relative path to locate ampush.conf

--- amlib/conf.py
+++ amlib/conf.py
@@ -1,8 +1,15 @@
+import os
 from ConfigParser import ConfigParser
 
 ''' load config '''
 tmp_conf = ConfigParser()
-tmp_conf.read('ampush.conf')
+
+tmp_path = os.path.dirname(os.path.abspath(__file__))  # /base/lib/here
+tmp_path = tmp_path.split('/')
+conf_path = '/'.join(tmp_path[0:-1])   # /base/lib
+tmp_conf.read(conf_path+'/ampush.conf')
+
+#tmp_conf.read('ampush.conf')
 c = {}
 
 c.update(tmp_conf.items('default'))
